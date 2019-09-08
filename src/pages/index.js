e1f46f9a0558678025369f4e28fdfd4086ded102
Tarasov Oleg <otarasov@mentalstack.com>
2019-09-08 14:02:24 +0300

    v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import Hero from '../components/hero'
+import Layout from '../components/layout'
 import ArticlePreview from '../components/article-preview'
 
 class RootIndex extends React.Component {
@@ -10,23 +12,26 @@ class RootIndex extends React.Component {
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
     const [author] = get(this, 'props.data.allContentfulPerson.edges')
 
+    console.log('posts', posts, author)
+
     return (
-      <div style={{ background: '#fff' }}>
-        <Helmet title={siteTitle} />
-        <Hero data={author.node} />
-        <div className="wrapper">
-          <h2 className="section-headline">Recent articles</h2>
-          <ul className="article-list">
-            {posts.map(({ node }) => {
-              return (
-                <li key={node.slug}>
-                  <ArticlePreview article={node} />
-                </li>
-              )
-            })}
-          </ul>
+      <Layout location={this.props.location} >
+        <div style={{ background: '#fff' }}>
+          <Helmet title={siteTitle} />
+          <div className="wrapper">
+            <h2 className="section-headline">Recent articles</h2>
+            <ul className="article-list">
+              {posts.map(({ node }) => {
+                return (
+                  <li key={node.slug}>
+                    <ArticlePreview article={node} />
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      </Layout>
     )
   }
 }
@@ -35,6 +40,11 @@ export default RootIndex
 
 export const pageQuery = graphql`
   query HomeQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
       edges {
         node {
@@ -43,8 +53,8 @@ export const pageQuery = graphql`
           publishDate(formatString: "MMMM Do, YYYY")
           tags
           heroImage {
-            sizes(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-             ...GatsbyContentfulSizes_withWebp
+            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+             ...GatsbyContentfulFluid_tracedSVG
             }
           }
           description {
@@ -55,7 +65,7 @@ export const pageQuery = graphql`
         }
       }
     }
-    allContentfulPerson(filter: { id: { eq: "c15jwOBqpxqSAOy2eOO4S0m" } }) {
+    allContentfulPerson(filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }) {
       edges {
         node {
           name
@@ -64,13 +74,13 @@ export const pageQuery = graphql`
           }
           title
           heroImage: image {
-            sizes(
+            fluid(
               maxWidth: 1180
               maxHeight: 480
               resizingBehavior: PAD
               background: "rgb:000000"
             ) {
-              ...GatsbyContentfulSizes_withWebp
+              ...GatsbyContentfulFluid_tracedSVG
             }
           }
         }
