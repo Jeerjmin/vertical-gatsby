e479f51e8d770be1af60dc7a52846da23f1e4e1c
Tarasov Oleg <otarasov@mentalstack.com>
2019-09-11 22:58:32 +0300

    token

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || process.env.CONTENTFUL_DELIVERY_TOKEN ,
   host: process.env.CONTENTFUL_HOST
 }
 
