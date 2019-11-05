a760ab3d7653b6779feb70db1fcfa2f8b5daf19c
Tarasov <Jeerjmin@gmail.com>
2019-11-05 23:36:50 +0300

    f

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
     render() {
         const prefix = this.props.template ? '../' : ''
         return (
-            <section className={styles.container} style={{ maxWidth: 1718, background: 'black'}}>
+            <section className={styles.container} style={{background: 'black'}}>
                     <img src={prefix + "assets/footer.jpg"} alt="Контакты" />
                     <div className={styles.contacts}>info@vertical.spb.ru  /  +7 812.244.0158</div>
 
