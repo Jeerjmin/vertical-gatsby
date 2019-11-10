3707843c63f8e39f71f84f27121e4d87674bb9d5
Oleg Tarasov <otarasov@mentalstack.com>
2019-11-10 18:34:31 +0300

    fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,3 +11,13 @@ export const shouldUpdateScroll = ({
 
     return false
 }
+
+
+export const onInitialClientRender = () => {
+    // dirty fix for missing popstate listener
+    const GATSBY_NAVIGATE = window.___navigate || {}
+
+    window.addEventListener('popstate', () =>
+        GATSBY_NAVIGATE(window.location.pathname, { replace: true })
+    )
+}
